79d33423fbdea7447e534cc53ee1b6f034d04fb6
Nikita Kornasevich <nikita.kornasevich@nowadigit.com>
2021-08-14 14:19:24 +0300

    init commit

---
+++ config/mainconfig.js
@@ -0,0 +1,25 @@
+export const MainConfig = {
+    AppDetails: {
+        app_name: "Your App Name", 
+        app_description: "Your App Description Here",
+        app_identifier: "com.something.app",
+        ios_app_version: "1.0.0", 
+        android_app_version: 100
+    },
+    FirebaseConfig: {
+        apiKey: "AIzaSyAB7AXZhKSt2K-5r2ffRKRjgzg453FyIjs",
+        authDomain: "taxi135-dd2b3.firebaseapp.com",
+        databaseURL: "https://taxi135-dd2b3-default-rtdb.firebaseio.com",
+        projectId: "taxi135-dd2b3",
+        storageBucket: "taxi135-dd2b3.appspot.com",
+        messagingSenderId: "941873136458",
+        appId: "1:941873136458:web:db10c4492d0cb7133f77b3",
+        measurementId: "G-RD18V3Y3P1"
+    },
+    Google_Map_Key: "AIzaSyAB7AXZhKSt2K-5r2ffRKRjgzg453FyIjs",
+    facebookAppId: "XXXXXXXXXXXXXXXXX",
+    PurchaseDetails: {
+        CodeCanyon_Purchase_Code: "",
+        Buyer_Email_Address: ""
+    }
+}
